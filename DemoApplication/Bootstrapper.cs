e1d0352a8844e63c62c0abbba8cc20031928eda0
BItClever <neonzhlobin@tut.by>
2017-06-23 03:34:13 +0300

    First part
    Create WPF application, install Prism, MEF packages via nuget, create
    bootstrapper, add region to ShellView. Solution name Demo, shell project
    name Demo.Application

---
+++ DemoApplication/Bootstrapper.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Practices.ServiceLocation;
+using Prism.Mef;
+using Prism.Modularity;
+using Prism.Logging;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DemoApplication
+{
+    public class DemoBootstrapper : MefBootstrapper
+    {
+
+        protected override DependencyObject CreateShell()
+        {
+            return ServiceLocator.Current.GetInstance<ShellView>();
+            //return this.Container.GetExportedValue<ShellView>();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow = (ShellView) this.Shell;
+            Application.Current.MainWindow.Show();
+        }
+
+        protected override void ConfigureAggregateCatalog()
+        {
+            base.ConfigureAggregateCatalog();
+            AggregateCatalog.Catalogs.Add(new AssemblyCatalog(typeof(DemoBootstrapper).Assembly));
+        }
+
+        protected override IModuleCatalog CreateModuleCatalog()
+        {
+            return new ConfigurationModuleCatalog();
+        }
+    }
+}
---
+++ DemoApplication/ShellView.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Prism.Logging;
+using Prism.Modularity;
+using System.ComponentModel.Composition;
+
+namespace DemoApplication
+{
+    /// <summary>
+    /// Логика взаимодействия для ShellView.xaml
+    /// </summary>
+   [Export]
+    public partial class ShellView : Window
+    {
+        public ShellView()
+        {
+            InitializeComponent();
+        }
+    }
+}
